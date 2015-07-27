837bcfbca0be850ed6faf5d2df405572f2e61577
Artur Lobanov <artur.lobanov@desy.de>
2015-07-27 09:35:48 +0200

    Copy contents of tarball

---
+++ ROOT_Tutorial_2015/skeleton_macro.cxx
@@ -0,0 +1,18 @@
+#include "TH1F.h"
+#include "TH1D.h"
+#include "TFile.h"
+#include "TRandom3.h"
+#include "TTree.h"
+
+void skeleton_macro(){
+  cout << "Hello World" << endl;
+
+  /// Example of how to generate random numbers sampled from a Gaussian ////
+
+  //TRandom3* rand = new TRandom3(123456); // Creates a random number generator object
+  //for( int i = 0; i <= 50000; ++i){
+  //    cout << i << " " << rand->Gaus(0,1) << endl; // Gives you a random number sampled from a Gaussian with mean 0 and width 1
+  //  }
+
+  return;
+}
